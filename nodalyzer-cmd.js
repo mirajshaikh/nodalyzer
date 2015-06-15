4c9e039aa922fafe6c357aca2cabbeff60ff845f
Pinto, Lohith <lpinto@akamai.com>
2015-06-15 22:22:22 +0530

    Added command line scripts nodalyzer-cmd.js and server.js.

---
+++ nodalyzer-cmd.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+var nodalyzer = require('nodalyzer'),
+    minimist = require('minimist'),
+    argv = minimist(process.argv.slice(2)),
+    nport = argv.nport || 8125,
+    usage = function () {
+        console.log('Usage: nodalyzer [-options] -- url..');
+        console.log('-v, --verbose\t\tincrease verbose, default false');
+        console.log('    --nport\t\tport number to use for injecting js files, default 8125');
+        console.log('    --help\t\tprint this text and exit');
+        console.log();
+    };
+
+if (argv.help || argv._.length === 0) {
+    usage();
+    process.exit(0);
+}
+
+nodalyzer.init(function(status) {
+    if (status === true) {
+        argv._.forEach(function (url) {
+            nodalyzer.get(url, function (err, data) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(JSON.stringify(data));
+                }
+            });
+        });
+
+        nodalyzer.close();
+    } else {
+        console.error('Failed to init nodalyzer.');
+    }
+}, {
+    verbose: argv.verbose || argv.v || false,
+    nport: nport,
+});
---
+++ server.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+
+var nodalyzer = require('./lib/main'),
+    minimist = require('minimist'),
+    http = require('http'),
+    url = require('url'),
+    argv = minimist(process.argv.slice(2)),
+    port = argv.port || argv.p || 3000,
+    nport = argv.nport || 8125,
+    hostname = argv.hostname || argv.h || '127.0.0.1',
+    usage = function () {
+        console.log('Usage: server.js [-options]');
+        console.log('-h, --hostname\t\thostname, default 127.0.0.1');
+        console.log('-p, --port\t\tport number, default 3000');
+        console.log('    --nport\t\tport number to use for injecting js files, default 8125');
+        console.log('    --help\t\tprint this text and exit');
+        console.log();
+    };
+
+if (argv.help) {
+    usage();
+    process.exit(0);
+}
+
+nodalyzer.init(function(status) {
+    http.createServer(function (req, res) {
+        var query = url.parse(req.url, true).query;
+        nodalyzer.get(query.url, function (err, data) {
+            if (err) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end(err.toString());
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end(JSON.stringify(data));
+            }
+        });
+    }).listen(port, hostname);
+    console.log('Server running at http://' + hostname + ':' + port + '/');
+}, {
+    verbose: argv.verbose || argv.v || false,
+    nport: nport,
+});
